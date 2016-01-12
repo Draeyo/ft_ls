bcd08787e4adc16e0c12665a673618bb78943b6d
Draeyo <draeyo@gmail.com>
2016-01-12 12:19:32 +0100

    .h

---
+++ includes/ft_ls.h
@@ -0,0 +1,17 @@
+#ifndef FT_LS_H
+# define FT_LS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <dirent.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <pwd.h>
+# include <uuid/uuid.h>
+# include <grp.h>
+# include <sys/xattr.h>
+# include <time.h>
+# include <stdio.h>
+# include <string.h>
+
+# endif
