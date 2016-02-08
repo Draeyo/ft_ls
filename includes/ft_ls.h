00fdd035d42796a4092f8fad2b3ff351b9d9f82b
Draeyo <draeyo@gmail.com>
2016-02-08 14:25:00 +0100

    123456789

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -6,10 +6,11 @@
 # include <dirent.h>
 # include <sys/stat.h>
 # include <sys/types.h>
-# include <pwd.h>
-# include <uuid/uuid.h>
-# include <grp.h>
 # include <sys/xattr.h>
+# include <sys/dir.h>
+# include <uuid/uuid.h>
+# include <pwd.h>
+# include <grp.h>
 # include <time.h>
 # include <stdio.h>
 # include <string.h>
--- test/main.c
+++ test/main.c
@@ -1,86 +1,107 @@
 #include "ft_ls.h"
 
-char	*rights(struct stat file)
+static char		*fill_rights(char *str)
 {
-	char	*str;
+	int		i;
 
-	str = (char*)malloc(sizeof(*str) * 10 + 1);
-	if (S_ISDIR(file.st_mode))
-		str[0] = 'd';
-	else
-		str[0] = '-';
-	if (file.st_mode & S_IRUSR)
-		str[1] = 'r';
-	else
-		str[1] = '-';
-	if (file.st_mode & S_IWUSR)
-		str[2] = 'w';
-	else
-		str[2] = '-';
-	if (file.st_mode & S_IXUSR)
-		str[3] = 'x';
-	else
-		str[3] = '-';
-	if (file.st_mode & S_IRGRP)
-		str[4] = 'r';
-	else
-		str[4] = '-';
-	if (file.st_mode & S_IWGRP)
-		str[5] = 'w';
-	else
-		str[5] = '-';
-	if (file.st_mode & S_IXGRP)
-		str[6] = 'x';
-	else
-		str[6] = '-';
-	if (file.st_mode & S_IROTH)
-		str[7] = 'r';
-	else
-		str[7] = '-';
-	if (file.st_mode & S_IWOTH)
-		str[8] = 'w';
-	else
-		str[8] = '-';
-	if (file.st_mode & S_IXOTH)
-		str[9] = 'x';
-	else
-		str[9] = '-';
-	str[10] = '\0';
+	i = 0;
+	while (i++ != 10)
+		str[i] = '-';
 	return (str);
 }
 
-int	main(int ac, char **av)
+void			rights(struct stat file)
 {
-	void		*dir_fd;
+	char	*str;
+
+	str = ft_strnew(10);
+	str = fill_rights(str);
+	ft_putchar((S_ISDIR(file.st_mode) ? 'd' : '-'));
+	ft_putchar((file.st_mode & S_IRUSR ? 'r' : '-'));
+	ft_putchar((file.st_mode & S_IWUSR ? 'w' : '-'));
+	ft_putchar((file.st_mode & S_IXUSR ? 'x' : '-'));
+	ft_putchar((file.st_mode & S_IRGRP ? 'r' : '-'));
+	ft_putchar((file.st_mode & S_IWGRP ? 'w' : '-'));
+	ft_putchar((file.st_mode & S_IXGRP ? 'x' : '-'));
+	ft_putchar((file.st_mode & S_IROTH ? 'r' : '-'));
+	ft_putchar((file.st_mode & S_IWOTH ? 'w' : '-'));
+	ft_putchar((file.st_mode & S_IXOTH ? 'x' : '-'));
+	free(str);
+}
+
+void	show_mtime(const char *str)
+{
+	char	*month;
+	char	*day;
+	char	*hour;
+	char	*cpy;
+	int		i;
+
+	month = ft_strnew(3);
+	day = ft_strnew(2);
+	hour = ft_strnew(5);
+	cpy = (char*)str;
+	i = 0;
+	//ft_putstr(str);
+	cpy += 4;
+	write (1, cpy, ft_strlen(cpy) - 9);
+}
+
+int				main(int ac, char **av)
+{
+	void			*dir_fd;
 	struct group	*group;
-	int		closeret;
-	struct stat	file;
+	struct passwd	*passwd;
+	int				closeret;
+	struct stat		file;
 	struct dirent	*entry;
+	int				i;
+	int				blocks;
 	
-	if (ac != 2)
+	if (ac < 2)
 		return (0);
-	if (!(dir_fd = opendir(av[1])))
+	i = 0;
+	blocks = 0;
+	while (av[++i])
 	{
-		ft_putendl("Not a directory");
-		return (0);
-	}
-	while ((entry = readdir(dir_fd)))
-	{
-		stat(entry->d_name, &file);
-		ft_putstr(rights(file));
-		ft_putchar(' ');
-		group = getgrgid(file.st_gid);
-		ft_putstr(group->gr_name);
-		ft_putchar(' ');
-		ft_putnbr(file.st_size);
-		ft_putchar(' ');
-		ft_putstr(entry->d_name);
+		if (!(dir_fd = opendir(av[i])))
+		{
+			ft_putendl("Not a directory");
+			exit(EXIT_FAILURE);
+		}
+		ft_putchar((i > 1 ? '\n' : '\0'));
+		ft_putstr((ac > 2 ? av[i] : ""));
+		ft_putchar((ac > 2 ? ':' : '\0'));
+		ft_putchar((ac > 2 ? '\n' : '\0'));
+		while ((entry = readdir(dir_fd)))
+		{
+			stat(entry->d_name, &file);
+			rights(file);
+			ft_putstr("  ");
+			ft_putnbr(file.st_nlink);
+			ft_putstr(" ");
+			passwd = getpwuid(file.st_uid);
+			ft_putstr(passwd->pw_name);
+			ft_putstr("  ");
+			group = getgrgid(file.st_gid);
+			ft_putstr(group->gr_name);
+			ft_putstr("  ");
+			ft_putnbr(file.st_size);
+			ft_putstr("  ");
+			show_mtime(ctime(&file.st_mtimespec.tv_sec));
+			ft_putstr("  ");
+			ft_putstr(entry->d_name);
+			ft_putchar('\n');
+			blocks += file.st_blocks;
+		}
+		ft_putstr("total ");
+		ft_putnbr(blocks);
 		ft_putchar('\n');
-	}
-	if ((closeret = closedir(dir_fd)) != 0)
-	{
-		ft_putendl("Error on close");
-		return (0);
+		if ((closeret = closedir(dir_fd)) != 0)
+		{
+			ft_putendl("Error on close");
+			exit(EXIT_FAILURE);
+		}
 	}
 	return (0);
 }
